16dcd47bf7686b5c50516496346588cf28e432fb
Plinio Naves <pliniopjn@hotmail.com>
2023-09-17 12:58:03 -0300

    chore: add edge functions test

---
+++ server/api/upload/test-file.ts
@@ -0,0 +1,13 @@
+import { readFiles } from 'h3-formidable';
+
+const apiKey = process.env.INFURA_API_KEY;
+
+export default defineEventHandler(async (event) => {
+  const files = await readFiles(event);
+  const file = files.file[0];
+  const apiSecret = process.env.INFURA_API_SECRET;
+
+  console.log('test', 'ok');
+
+  return { ok: true, file: !!file, env1: !!apiKey, env2: !!apiSecret };
+});
