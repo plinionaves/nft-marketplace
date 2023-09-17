d4e9459c433cf333d8a7eda0d0fff5ea98582160
Plinio Naves <pliniopjn@hotmail.com>
2023-09-17 13:49:19 -0300

    fix: fix file upload

--- server/api/upload/file.ts
+++ server/api/upload/file.ts
@@ -1,31 +1,35 @@
-import fs from 'fs';
-import { readFiles } from 'h3-formidable';
-import { StorageData, client } from './data';
+import { client } from './data';
 
 export default defineEventHandler(async (event) => {
-  const files = await readFiles(event);
-  const file = files.file[0];
+  const files = await readMultipartFormData(event);
+  const file = files?.[0];
 
   if (!file) {
-    throw new Error('No file');
+    throw createError({
+      statusCode: 400,
+      message: 'No file provided',
+      stack: undefined,
+    });
   }
 
-  return new Promise<StorageData>(async (resolve, reject) => {
-    const blobParts: Buffer[] = [];
+  // only allow images
+  if (!file.type?.startsWith('image/')) {
+    throw createError({
+      statusCode: 400,
+      message: 'Only images are allowed',
+      stack: undefined,
+    });
+  }
 
-    fs.createReadStream(file.filepath)
-      .on('data', (chunk) => {
-        blobParts.push(Buffer.from(chunk));
-      })
-      .on('end', async () => {
-        const blob = new Blob(blobParts);
+  if (file.data.length > 0.5 * 1024 * 1024) {
+    throw createError({
+      statusCode: 400,
+      message: 'File size must be less than 5MB',
+      stack: undefined,
+    });
+  }
 
-        const { cid, path, size } = await client.add(blob);
+  const { cid, path, size } = await client.add(file.data);
 
-        resolve({ cid, path, size });
-      })
-      .on('error', (error) => {
-        reject(error);
-      });
-  });
+  return { cid, path, size };
 });
