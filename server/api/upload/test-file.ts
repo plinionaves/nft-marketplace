5af06853c0a4c5ce1499e21442e012ec5dc559e5
Plinio Naves <pliniopjn@hotmail.com>
2023-09-17 13:26:07 -0300

    chore: wip

--- server/api/upload/test-file.ts
+++ server/api/upload/test-file.ts
@@ -3,11 +3,11 @@ import { readFiles } from 'h3-formidable';
 const apiKey = process.env.INFURA_API_KEY;
 
 export default defineEventHandler(async (event) => {
-  // const files = await readFiles(event);
+  const files = await readFiles(event);
   // const file = files.file[0];
   const apiSecret = process.env.INFURA_API_SECRET;
 
   console.log('test', 'ok');
 
-  return { ok: true, env1: !!apiKey, env2: !!apiSecret };
+  return { ok: true, env1: !!apiKey, env2: !!apiSecret, files: !!files };
 });
